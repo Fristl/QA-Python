8184cc9811f28c343329eccd207813caf154eacf
Vladislav Ruban <SPEEDYYY13@yandex.ru>
2025-06-10 14:28:53 +0300

    fix(qa-3): some refactoring

--- src/figure.py
+++ src/figure.py
@@ -2,6 +2,7 @@
 
 from abc import ABC
 from abc import abstractmethod
+from functools import cached_property
 
 from common.exceptions import FigureError
 from common.exceptions import SizeError
@@ -23,7 +24,8 @@ class Figure(ABC):
     def _area(self) -> SquareUnit:
         """Calculate the area of the figure."""
 
-    def get_area(self) -> SquareUnit:
+    @cached_property
+    def area(self) -> SquareUnit:
         """Return the area of the figure."""
         return self._area()
 
@@ -31,13 +33,13 @@ class Figure(ABC):
     def _perimeter(self) -> Number:
         """Calculate the perimeter of the figure."""
 
-    def get_perimeter(self) -> Number:
+    @property
+    def perimeter(self) -> Number:
         """Return the perimeter of the figure."""
         return self._perimeter()
 
     def add_area(self, figure: "Figure") -> SquareUnit:
         """Summary the area of self figure and another figure."""
         if isinstance(figure, Figure):
-            return self.get_area() + figure.get_area()
+            return self.area + figure.area
         raise FigureError(figure)
-
